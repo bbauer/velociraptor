0c0f35bcedc61a3e8a48629b8c1d15e382436590
bbauer <brian.bauer@g5platform.com>
2014-01-12 19:03:01 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0.beta'
+  gem 'pry'
+  gem 'pry-rails'
+  gem 'pry-theme'
+  gem 'pry-debugger'
+  gem 'awesome_print'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Velociraptor::Application.config.secret_key_base = 'f7ee0df83c4b65f18ed7adbc86f791eecbfa1c5b12419b1acabfc2d821de45618e0b8d9a0a6a0a0eaa3b739d949de0873f7168d30cb6eafd2616f425e0028e20'
